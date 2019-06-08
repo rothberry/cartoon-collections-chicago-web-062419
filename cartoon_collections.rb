2aafb7de79eaabdf2bce32d5ab030e0cf92a921d
Phil Roth <phil.roth077@gmail.com>
2019-06-08 18:38:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,13 @@ def long_planeteer_calls(words)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |i|
+    i == cheese_types[i]
+    i+=1
+  end
+
+  
 end
