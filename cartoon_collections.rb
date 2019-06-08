ed4b626bfb8d17fc781708922248953ed4fa9ef2
Phil Roth <phil.roth077@gmail.com>
2019-06-08 18:29:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect { |chr| chr.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
